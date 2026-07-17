548957a3f9b1d4214c42c2b903e2fb4623b2192c
Aruuke Turgunbaeva <nyc.aruu@gmail.com>
2026-07-17 16:14:13 +0200

    Initialize Next.js frontend and configure CORS

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from app.database import client, database
 from app.routers import videos
 from app.routers import export
+from fastapi.middleware.cors import CORSMiddleware
 
 # Lifespan
 @asynccontextmanager
@@ -20,6 +21,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="UniMe Video Catalog API",
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(videos.router)
